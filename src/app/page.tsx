70a007e7c25fc178fe46fd361dd52d8fa5128cdc
Yusuf Kahya <hi@yimikami.me>
2024-07-02 19:46:02 +0300

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -218,7 +218,7 @@ export default function Page() {
                   href={DATA.contact.social.X.url}
                   className="text-blue-500 hover:underline"
                 >
-                  with a direct question on X
+                  with a direct question on Twitter
                 </Link>{" "}
                 and I&apos;ll respond whenever I can. I will ignore all
                 soliciting.
